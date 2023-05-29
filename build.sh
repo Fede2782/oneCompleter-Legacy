0e7bcb0114fa69fbb93639bc36f2265e54010935
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2023-05-29 14:06:19 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ rm *.zip
 version="$(grep '^version=' module.prop  | cut -d= -f2)"
 zip -r9 "oneCompleter-$version.zip" . -x build.sh update.json
 zip --delete "oneCompleter-$version.zip" ".git/*" || true
-
+zip --delete "oneCompleter-$version.zip" ".github/*" || true
 
