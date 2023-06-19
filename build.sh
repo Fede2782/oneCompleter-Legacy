d8a8f6c9c4a5e8e3a7a900e7b7e81832c2478b98
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2023-06-19 18:41:17 +0200

    New logo (#15)
    * Create nothing
    
    * Add files via upload
    
    * Add files via upload
    
    * Update README.md
    
    * Update module.yml
    
    * Update build.sh
    
    * Delete nothing

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ version="$(grep '^version=' module.prop  | cut -d= -f2)"
 zip -r9 "oneCompleter-$version.zip" . -x build.sh update.json
 zip --delete "oneCompleter-$version.zip" ".git/*" || true
 zip --delete "oneCompleter-$version.zip" ".github/*" || true
+zip --delete "oneCompleter-$version.zip" "img/*" || true
 
