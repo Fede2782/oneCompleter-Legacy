87945017d1e12a292cda4f6cba1cf4912f917de0
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2023-07-14 20:12:31 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,10 +6,43 @@ zip --delete "oneCompleter-$version.zip" ".git/*" || true
 zip --delete "oneCompleter-$version.zip" ".github/*" || true
 zip --delete "oneCompleter-$version.zip" "img/*" || true
 
+MODPATH=$(pwd)
+
+function download_offline {
+  mkdir $MODPATH/tmp/
+  
+  wget -O $MODPATH/tmp/AREmojiEditor.tar.gz "https://gitlab.com/Fede2782/onecompleter-files/-/raw/main/AREmojiEditor.tar.gz"
+  mkdir $MODPATH/system/priv-app/AREmojiEditor/
+  tar -xvf $MODPATH/tmp/AREmojiEditor.tar.gz -C $MODPATH/system/priv-app/AREmojiEditor/
+
+  wget -O $MODPATH/tmp/AvatarEmojiSticker.tar.gz "https://gitlab.com/Fede2782/onecompleter-files/-/raw/main/AvatarEmojiSticker.tar.gz"
+  mkdir $MODPATH/system/priv-app/AvatarEmojiSticker/
+  tar -xvf $MODPATH/tmp/AvatarEmojiSticker.tar.gz -C $MODPATH/system/priv-app/AvatarEmojiSticker/
+  
+  wget -O $MODPATH/tmp/AREmoji.tar.gz "https://gitlab.com/Fede2782/onecompleter-files/-/raw/main/AREmoji.tar.gz"
+  mkdir $MODPATH/system/priv-app/AREmoji/
+  tar -xvf $MODPATH/tmp/AREmoji.tar.gz -C $MODPATH/system/priv-app/AREmoji/
+
+  wget -O $MODPATH/tmp/SamsungSmartSuggestions.tar.gz "https://gitlab.com/Fede2782/onecompleter-files/-/raw/main/SamsungSmartSuggestions.tar.gz"
+  mkdir $MODPATH/system/priv-app/SamsungSmartSuggestions/
+  tar -xvf $MODPATH/tmp/SamsungSmartSuggestions.tar.gz -C $MODPATH/system/priv-app/SamsungSmartSuggestions/
+
+  wget -O $MODPATH/tmp/FunModeSDK.tar.gz "https://gitlab.com/Fede2782/onecompleter-files/-/raw/main/FunModeSDK.tar.gz"
+  mkdir $MODPATH/system/app/FunModeSDK/
+  tar -xvf $MODPATH/tmp/FunModeSDK.tar.gz -C $MODPATH/system/app/FunModeSDK/
+  
+  wget -O $MODPATH/tmp/SamsungWeather.tar.gz "https://gitlab.com/Fede2782/onecompleter-files/-/raw/main/SamsungWeather.tar.gz"
+  tar -xvf $MODPATH/tmp/SamsungWeather.tar.gz -C $MODPATH/system/app/SamsungWeather/
+
+  wget -O $MODPATH/tmp/EditorFiles.tar.gz "https://gitlab.com/Fede2782/onecompleter-files/-/raw/main/EditorFiles.tar.gz"
+  tar -xvf $MODPATH/tmp/EditorFiles.tar.gz -C $MODPATH/system/etc/
+
+}
+
 if [[ "$1" == "offline" ]]; then
     echo "Preparing offline installer..."
     download_offline
-    zip -r9 "oneCompleter-$version-offline.zip" . -x build.sh update.json
+    zip -r9 "oneCompleter-$version-offline.zip" . -x build.sh update.json "oneCompleter-$version.zip"
     zip --delete "oneCompleter-$version-offline.zip" ".git/*" || true
     zip --delete "oneCompleter-$version-offline.zip" ".github/*" || true
     zip --delete "oneCompleter-$version-offline.zip" "img/*" || true
