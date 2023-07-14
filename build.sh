79bbe5d45cd7e50f7f8ce14ec5fc7c51b468724a
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2023-07-14 19:41:52 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,13 @@ zip --delete "oneCompleter-$version.zip" ".git/*" || true
 zip --delete "oneCompleter-$version.zip" ".github/*" || true
 zip --delete "oneCompleter-$version.zip" "img/*" || true
 
+if [[ "$1" == "offline" ]]; then
+    echo "Preparing offline installer..."
+    download_offline
+    zip -r9 "oneCompleter-$version-offline.zip" . -x build.sh update.json
+    zip --delete "oneCompleter-$version-offline.zip" ".git/*" || true
+    zip --delete "oneCompleter-$version-offline.zip" ".github/*" || true
+    zip --delete "oneCompleter-$version-offline.zip" "img/*" || true
+else
+    echo "Completed"
+fi
