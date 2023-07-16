37bab3d532bc55afd55d2b89f78ef497809a784b
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2023-07-16 09:15:46 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 rm *.zip
 version="$(grep '^version=' module.prop  | cut -d= -f2)"
-zip -r9 "oneCompleter-$version.zip" . -x build.sh update.json
-zip --delete "oneCompleter-$version.zip" ".git/*" || true
-zip --delete "oneCompleter-$version.zip" ".github/*" || true
-zip --delete "oneCompleter-$version.zip" "img/*" || true
+zip -r9 "oneCompleter-$version-online.zip" . -x build.sh update.json
+zip --delete "oneCompleter-$version-online.zip" ".git/*" || true
+zip --delete "oneCompleter-$version-online.zip" ".github/*" || true
+zip --delete "oneCompleter-$version-online.zip" "img/*" || true
 
 MODPATH=$(pwd)
 
@@ -43,7 +43,7 @@ if [[ "$1" == "offline" ]]; then
     echo "Preparing offline installer..."
     mv customize_offline.sh customize.sh
     download_offline
-    zip -r9 "oneCompleter-$version-offline.zip" . -x build.sh update.json "oneCompleter-$version.zip" customize_offline.sh
+    zip -r9 "oneCompleter-$version-offline.zip" . -x build.sh update.json "oneCompleter-$version-online.zip" customize_offline.sh
     zip --delete "oneCompleter-$version-offline.zip" ".git/*" || true
     zip --delete "oneCompleter-$version-offline.zip" ".github/*" || true
     zip --delete "oneCompleter-$version-offline.zip" "img/*" || true
