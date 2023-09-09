3bbbc85416209b6a83fdc6c6cfe0f547c6a8cfa2
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2023-09-09 22:08:46 +0200

    Improve stability (make sure SecondScreen and SmartWidget don't disable)

--- customize_offline.sh
+++ customize_offline.sh
@@ -29,6 +29,24 @@ unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH
 ui_print "- Force enabling features: SecondScreen and SmartSuggestions..."
 chmod +x $MODPATH/service.sh
 
+qsettings=$(settings get secure sysui_qs_tiles)
+
+pm disable com.samsung.android.smartmirroring/com.samsung.android.smartmirroring.settings.DisableSecondScreenActivity
+pm enable com.samsung.android.smartmirroring/com.samsung.android.smartmirroring.player.SecondScreenActivity
+pm enable com.samsung.android.smartmirroring/com.samsung.android.smartmirroring.tile.ScreenSharingTile
+pm enable com.samsung.android.smartsuggestions/com.samsung.android.smartsuggestions.widget.appwidget.SmartSuggestionsWidgetProvider
+pm enable com.samsung.android.smartmirroring/.player.SecondScreenActivity
+pm enable com.samsung.android.smartmirroring/.tile.ScreenSharingTile
+
+ui_print "- Adding Second Screen tile in Quick Settings..."
+ui_print "- If you remove it you may have to install again the module (you don't need to uninstall it first)"
+if [[ $qsettings == *"ScreenSharing"* ]]; then
+  echo "The string contains 'ScreenSharing'. No further action needed." >> /dev/null
+else
+  # Add your additional actions here
+  settings put secure sysui_qs_tiles "$qsettings,custom(com.samsung.android.smartmirroring/.tile.ScreenSharingTile)"
+fi
+
 ui_print "- Now clearing temp files and system cache to make everything working..."
 rm -rf /data/system/package_cache/*
 
--- service.sh
+++ service.sh
@@ -1,7 +1,19 @@
 qsettings=$(settings get secure sysui_qs_tiles)
-if [[ $qsettings != *ScreenSharing* ]]; then
-    settings put secure sysui_qs_tiles "$qsettings,custom(com.samsung.android.smartmirroring/.tile.ScreenSharingTile)"
+
+pm disable com.samsung.android.smartmirroring/com.samsung.android.smartmirroring.settings.DisableSecondScreenActivity
+pm enable com.samsung.android.smartmirroring/com.samsung.android.smartmirroring.player.SecondScreenActivity
+pm enable com.samsung.android.smartmirroring/com.samsung.android.smartmirroring.tile.ScreenSharingTile
+pm enable com.samsung.android.smartsuggestions/com.samsung.android.smartsuggestions.widget.appwidget.SmartSuggestionsWidgetProvider
+pm enable com.samsung.android.smartmirroring/.player.SecondScreenActivity
+pm enable com.samsung.android.smartmirroring/.tile.ScreenSharingTile
+
+ui_print "- Adding Second Screen tile in Quick Settings..."
+ui_print "- If you remove it you may have to install again the module (you don't need to uninstall it first)"
+if [[ $qsettings == *"ScreenSharing"* ]]; then
+  echo "The string contains 'ScreenSharing'. No further action needed." >> /dev/null
 else
-    sleep 1
+  # Add your additional actions here
+  settings put secure sysui_qs_tiles "$qsettings,custom(com.samsung.android.smartmirroring/.tile.ScreenSharingTile)"
 fi
-watch -n 60 pm enable com.samsung.android.smartsuggestions/com.samsung.android.smartsuggestions.widget.appwidget.SmartSuggestionsWidgetProvider
+
+watch -n 45 pm enable com.samsung.android.smartsuggestions/com.samsung.android.smartsuggestions.widget.appwidget.SmartSuggestionsWidgetProvider >> /dev/null
