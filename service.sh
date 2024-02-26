4b53b6199629da699e3e4e029b4f94620d76c98b
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2024-02-26 21:26:06 +0100

    Make sure everything is ready

--- service.sh
+++ service.sh
@@ -1,3 +1,8 @@
+# Loop fino a quando ro.boot.completed non è uguale a 1
+while [ "$(getprop ro.boot.completed)" != "1" ]; do
+    sleep 1
+done
+
 qsettings=$(settings get secure sysui_qs_tiles)
 
 pm disable com.samsung.android.smartmirroring/com.samsung.android.smartmirroring.settings.DisableSecondScreenActivity
@@ -7,8 +12,6 @@ pm enable com.samsung.android.smartmirroring/.player.SecondScreenActivity
 pm enable com.samsung.android.smartmirroring/.tile.ScreenSharingTile
 pm enable com.samsung.android.smartsuggestions/com.samsung.android.smartsuggestions.widget.appwidget.SmartSuggestionsWidgetProvider
 
-ui_print "- Adding Second Screen tile in Quick Settings..."
-ui_print "- If you remove it you may have to install again the module (you don't need to uninstall it first)"
 if [[ $qsettings == *"ScreenSharing"* ]]; then
   echo "The string contains 'ScreenSharing'. No further action needed." >> /dev/null
 else
